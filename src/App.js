83e51e07cee16d980b942816915bf30b5ade8e8a
reliance <reliance@jinhakapply.com>
2023-03-27 15:53:56 +0900

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const cards = ["봄", "여름", "가을", "겨울"];
+  const cardlist = cards.map((card, index) => (
+    <div className="card" key={index}>
+      {card}
+    </div>
+  ));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="header">헤더부분</div>
       </header>
+      <div className="body">{cardlist}</div>
     </div>
   );
 }
